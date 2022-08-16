286fe7d17acfd1ea3806281bb7fd8c04f8d11d92
TH Huang <tzhsn.huang@gmail.com>
2022-08-16 16:10:52 -0500

    add a link from article to list

--- src/components/article/Header.jsx
+++ src/components/article/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = (props) => {
   return (
     <div className="article__header">
       <section className="article__title">
-        {(props.tag ? '[' + props.tag + '] ' : '') + props.title}
+        <div>{(props.tag ? '[' + props.tag + '] ' : '') + props.title}</div>
+        <Link to="/">
+          <div className="article__board_id">&lt;thtimes&gt;</div>
+        </Link>
       </section>
       <section className="article__info">
         ‣ {props.author} · {props.time.replace('T', ' ').slice(0, 19)}
