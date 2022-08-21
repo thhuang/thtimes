ad600e153c23fb2dc950606567c8fb9f70e2c06a
TH Huang <tzhsn.huang@gmail.com>
2022-08-21 18:44:27 -0500

    remove unused codes

--- src/assets/articles/articles.js
+++ src/assets/articles/articles.js
@@ -1,5 +1,3 @@
-// import { faker } from '@faker-js/faker';
-
 const articles = [];
 for (let i = 1; true; ++i) {
   try {
@@ -8,17 +6,6 @@ for (let i = 1; true; ++i) {
     break;
   }
 }
-
-// for (let i = 3; i <= 100; i++) {
-//   articles.push({
-//     id: i,
-//     date: faker.date.future().toISOString().slice(0, 10),
-//     author: faker.name.firstName().toLowerCase(),
-//     title: faker.random.words(4),
-//     tag: faker.random.alpha(4),
-//   });
-// }
-
 articles.reverse();
 
 export default articles;
